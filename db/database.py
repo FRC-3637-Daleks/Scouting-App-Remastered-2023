832b700697967c2bcc664f0b9a93c6b896dda3ae
PastForward <aronkt3458@hotmail.com>
2022-02-04 14:16:37 -0500

    added step 3 of readme

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from mysql.connector import (connection)
 from mysql.connector import errorcode
 
 try:
-    cnx = mysql.connector.connect(user="test", password="3637",
+    cnx = mysql.connector.connect(user="dalek", password="3637",
                             host="127.0.0.1")
 
     mycursor = cnx.cursor()
