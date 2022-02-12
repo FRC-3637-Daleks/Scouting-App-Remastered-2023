802a14dfb933f5fd0a4c936454ef9346b94726af
PastForward <aronkt3458@hotmail.com>
2022-02-12 08:52:28 -0500

    renamed tables.py

--- db/database.py
+++ db/database.py
@@ -2,9 +2,12 @@ import mysql.connector
 from mysql.connector import (connection)
 from mysql.connector import errorcode
 
-
 cnx = mysql.connector.connect(user="dalek", password="3637",
                         host="127.0.0.1",
                         database='Scouting')
 
+cursor = cnx.cursor()
+
+
+cursor.close()
 cnx.close()
