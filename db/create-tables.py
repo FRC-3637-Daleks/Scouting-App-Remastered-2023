265e4bae759f7e1485e2a34917dbd9ae4105b488
PastForward <aronkt3458@hotmail.com>
2022-02-12 08:51:57 -0500

    finished datatables with errorchecking

--- db/create-tables.py
+++ db/create-tables.py
@@ -1,51 +1,60 @@
 from __future__ import print_function
-
 import mysql.connector
 from mysql.connector import errorcode
 
 DB_NAME = 'Scouting'
 
 TABLES = {}
-TABLES['Scouting'] = (
+TABLES['Auton'] = (
     "CREATE TABLE `Auton` ("
-    "`Team` VARCHAR(10) NOT NULL, "
-    "`High` int NOT NULL, "
-    "`Low` int NOT NULL, "
-    "`Missed` int NOT NULL, "
-    "`Off_Platform` VARCHAR(1) NOT NULL, "
-    "`Basketball_Shots_Made` int NOT NULL, "
+    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `High` int NOT NULL,"
+    "   `Low` int NOT NULL,"
+    "   `Missed` int NOT NULL,"
+    "   `Off_Platform` VARCHAR(1) NOT NULL,"
+    "   `Basketball_Shots_Made` int NOT NULL,"
+    "   PRIMARY KEY (`Team`)"
+    ")"
 )
 
 TABLES['Teleop'] = (
     "CREATE TABLE `Teleop` ("
-    "`Team` VARCHAR(10) NOT NULL, "
-    "`High` int NOT NULL, "
-    "`Low` int NOT NULL, "
-    "`Missed` int NOT NULL, "
-    "`Burst` int NOT NULL, "
-    "`Launchpad` VARCHAR(1) NOT NULL, "
+    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `High` int NOT NULL,"
+    "   `Low` int NOT NULL,"
+    "   `Missed` int NOT NULL,"
+    "   `Burst` int NOT NULL,"
+    "   `Launchpad` VARCHAR(1) NOT NULL,"
+    "   PRIMARY KEY (`Team`)"
+    ")"
 )
 
 TABLES['Defense'] = (
     "CREATE TABLE `Defense` ("
-    "`Team` VARCHAR(10) NOT NULL, "
-    "`Blocked` VARCHAR(1) NOT NULL, "
-    "`Held_balls` VARCHAR(1) NOT NULL, "
+    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Blocked` VARCHAR(1) NOT NULL,"
+    "   `Held_balls` VARCHAR(1) NOT NULL,"
+    "   PRIMARY KEY (`Team`)"
+    ")"
 )
 
 TABLES['Endgame'] = (
     "CREATE TABLE `Endgame` ("
-    "`Team` VARCHAR(10) NOT NULL, "
-    "`Attempted_Climb` VARCHAR(1) NOT NULL, "
-    "`Success_Tier` int NOT NULL, "
-    "`Prepared` VARCHAR(1) NOT NULL, "
-    "`Climbing_Seconds` int NOT NULL, "
+    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Attempted_Climb` VARCHAR(1) NOT NULL,"
+    "   `Success_Tier` int NOT NULL,"
+    "   `Prepared` VARCHAR(1) NOT NULL,"
+    "   `Climbing_Seconds` int NOT NULL,"
+    "   PRIMARY KEY (`Team`)"    
+    ")"
 )
 
 TABLES['Comments'] = (
     "CREATE TABLE `Comments` ("
-    "`Team` VARCHAR(10) NOT NULL, "
-    "`Insert_Comments` MEDIUMTEXT, "
+    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Insert_Comments` MEDIUMTEXT,"
+    "   PRIMARY KEY (`Team`)"
+    ")"
 )
 
 cnx = mysql.connector.connect(user="dalek", password="3637",
