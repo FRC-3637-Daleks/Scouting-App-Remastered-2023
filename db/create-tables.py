4da758dbcf28f1534dcbfa3d19d87f1bbc5080ed
PastForward <aronkt3458@hotmail.com>
2022-02-22 19:53:12 -0500

    stable working version of creation of tables

--- db/create-tables.py
+++ db/create-tables.py
@@ -4,56 +4,56 @@ from mysql.connector import errorcode
 
 DB_NAME = 'Scouting'
 
+#todo: change primary key to also have a match and have match be put in manually like Team.
+    # aka MATCH 1, Match 2, Match 3 by user input
+    # add primary key above ")" looking like this:
+    # "     PRIMARY KEY (`Team`, `Match`)"
+
 TABLES = {}
 TABLES['Auton'] = (
     "CREATE TABLE `Auton` ("
-    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Team` VARCHAR(10) NOT NULL PRIMARY KEY,"
     "   `High` int,"
     "   `Low` int,"
     "   `Missed` int,"
     "   `Off_Platform` VARCHAR(1),"
-    "   `Basketball_Shots_Made` int"
-    "   PRIMARY KEY (`Team`)"
+    "   `Basketball_Shots_Made` VARCHAR(1)"
     ")"
 )
 
 TABLES['Teleop'] = (
     "CREATE TABLE `Teleop` ("
-    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Team` VARCHAR(10) NOT NULL PRIMARY KEY,"
     "   `High` int,"
     "   `Low` int,"
     "   `Missed` int,"
     "   `Burst` int,"
     "   `Launchpad` VARCHAR(1)"
-    "   PRIMARY KEY (`Team`)"
     ")"
 )
 
 TABLES['Defense'] = (
     "CREATE TABLE `Defense` ("
-    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Team` VARCHAR(10) NOT NULL PRIMARY KEY,"
     "   `Blocked` VARCHAR(1),"
     "   `Held_balls` VARCHAR(1)"
-    "   PRIMARY KEY (`Team`)"
     ")"
 )
 
 TABLES['Endgame'] = (
     "CREATE TABLE `Endgame` ("
-    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Team` VARCHAR(10) NOT NULL PRIMARY KEY,"
     "   `Attempted_Climb` VARCHAR(1),"
     "   `Success_Tier` int,"
     "   `Prepared` VARCHAR(1),"
-    "   `Climbing_Seconds` int"
-    "   PRIMARY KEY (`Team`)"
+    "   `Climbing_Seconds` VARCHAR(1)"
     ")"
 )
 
 TABLES['Comments'] = (
     "CREATE TABLE `Comments` ("
-    "   `Team` VARCHAR(10) NOT NULL,"
+    "   `Team` VARCHAR(10) NOT NULL PRIMARY KEY,"
     "   `Insert_Comments` MEDIUMTEXT"
-    "   PRIMARY KEY (`Team`)"
     ")"
 )
 
@@ -75,6 +75,7 @@ for table_name in TABLES:
             print(err.msg)
     else:
         print("OK")
+        
 
 cursor.close()
 cnx.close()
