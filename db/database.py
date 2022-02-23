410fbf7cb1c47ae9f065133c53249dbe89749193
PastForward <aronkt3458@hotmail.com>
2022-02-23 00:57:14 -0500

    included values to get new matchNumber + added errorchecking for executing mysql instead of reporting nothing

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-print ("Content-type: text/plain\r\n\r\n")
-print ("Hello World!")
+print ("Content-type: text/html\r\n\r\n")
+print ("<h1>Hello World!</h1>")
 
 
 import mysql.connector
@@ -26,6 +26,9 @@ print ("<p>", newData, "</p>")
 #get team number from form
 TEAM=data.getvalue('teamId')
 print ("<p>The team number is %s</p>" % TEAM)
+
+#get match number
+MN=data.getvalue('matchNumber')
 
 #auton high shots
 AHS=data.getvalue('AHS')
@@ -71,8 +74,8 @@ COMMENTS = data.getvalue('comments')
 
 try:
  
-    add_Auton = "INSERT INTO Auton (Team, High, Low, Missed, Off_Platform, Basketball_Shots_Made) VALUES (%s, %s, %s, %s, %s, %s)"
-    cursor.execute(add_Auton, (TEAM, AHS, ALS, ABM, AP, AB))
+    add_Auton = "INSERT INTO Auton (Team, Match_Number, High, Low, Missed, Off_Platform, Basketball_Shots_Made) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(add_Auton, (TEAM, MN, AHS, ALS, ABM, AP, AB))
 
 except:
     print ("<p>Error adding Auton data</p>")
@@ -81,8 +84,8 @@ finally:
 
     try:
 
-        add_Teleop = "INSERT INTO Teleop (Team, High, Low, Missed, Burst, Launchpad) VALUES (%s, %s, %s, %s, %s, %s)"
-        cursor.execute(add_Teleop, (TEAM, THS, TLS, TBM, TBS, TLP))
+        add_Teleop = "INSERT INTO Teleop (Team, Match_Number, High, Low, Missed, Burst, Launchpad) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(add_Teleop, (TEAM, MN, THS, TLS, TBM, TBS, TLP))
 
     except:
         print ("<p>Error adding Teleop data</p>")
@@ -91,8 +94,8 @@ finally:
 
         try:
 
-            add_Endgame = "INSERT INTO Endgame (Team, Attempted_Climb, Success_Tier, Prepared, Climbing_Seconds) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(add_Endgame, (TEAM, EAC, ECT, EPE, ECXS))
+            add_Endgame = "INSERT INTO Endgame (Team, Match_Number, Attempted_Climb, Success_Tier, Prepared, Climbing_Seconds) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(add_Endgame, (TEAM, MN, EAC, ECT, EPE, ECXS))
 
         except:
             print ("<p>Error adding Endgame data</p>")
@@ -101,8 +104,8 @@ finally:
 
             try:
 
-                add_Defense = "INSERT INTO Defense (Team, Blocked, Held_balls) VALUES (%s, %s, %s)"
-                cursor.execute(add_Defense, (TEAM, DAB, DHB))
+                add_Defense = "INSERT INTO Defense (Team, Match_Number, Blocked, Held_balls) VALUES (%s, %s, %s, %s)"
+                cursor.execute(add_Defense, (TEAM, MN, DAB, DHB))
 
             except:
                 print ("<p>Error adding Defense data</p>")
@@ -111,15 +114,15 @@ finally:
                 
                 try:
 
-                    add_Comments = "INSERT INTO Comments (Team, Insert_Comments) VALUES (%s, %s)"
-                    cursor.execute(add_Comments, (TEAM, COMMENTS))
+                    add_Comments = "INSERT INTO Comments (Team, Match_Number, Insert_Comments) VALUES (%s, %s, %s)"
+                    cursor.execute(add_Comments, (TEAM, MN, COMMENTS))
 
                 except:
                     print ("<p>Error adding Comments data</p>")
 
                 finally:
 
-                    print("IT NO WORKY")
+                    print("<p>DONE :D</p>")
                     cnx.commit()
                     cursor.commit()
                     cursor.close()
