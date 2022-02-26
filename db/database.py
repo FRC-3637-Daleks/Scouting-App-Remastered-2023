2c53c11fad942002567477f08d03e98b05694d85
PastForward <aronkt3458@hotmail.com>
2022-02-25 21:42:54 -0500

    added redirect url that still needs work

--- db/database.py
+++ db/database.py
@@ -2,6 +2,8 @@
 
 print ("Content-type: text/html\r\n\r\n")
 print ("<h1>Hello World!</h1>")
+redirectURL = "http://127.0.0.1/display/display.html"
+print('    <meta http-equiv="refresh" content="5;url='+str(redirectURL)+'" />') 
 
 
 import mysql.connector
