8bba408a3538787921b3f5f1162044e9bd6f7f75
PastForward <aronkt3458@hotmail.com>
2022-03-04 14:52:29 -0500

    turned into jquery

--- jquery-3.6.0.js
+++
@@ -1,8 +0,0 @@
-var slider = document.getElementById("teleopShotsMade");
-var output = document.getElementById("teleopB");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-output.innerHTML = this.value;
-}
