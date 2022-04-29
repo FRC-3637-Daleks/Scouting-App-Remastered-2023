02425d2f589aca2799f7cf3cc313685d1e34172d
Aron Kabai-Tokes <aronkt3458@gmail.com>
2022-04-29 18:40:44 -0400

    fixed issue of having to manually change the redirect link for each ip by doing it only through /

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,7 +1,7 @@
 # make sure to move this to [where it goes]
 
 # import the flask class
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flaskext.mysql import MySQL
 
 # create an instance of the flask class 
@@ -30,7 +30,7 @@ conn = mysql.connect()
 
 # define the route() decorator to link with a valid URL in the application
 #keep it as /, since its alias is changed in the 000-default.conf in sites-enabled.
-@app.route('/display')
+@app.route('/display') 
 
 # # define a function that is triggered when this URL appears in the browser address bar
 def dropdown(): 
@@ -53,28 +53,6 @@ def dropdown():
     
      #render template and send the set of tuples to the HTML file for displaying
     return render_template("display.html",auton_1=auton_1, comments_1=comments_1, defense_1=defense_1, endgame_1=endgame_1, teleop_1=teleop_1)
-
-# @app.route('/display',methods=["POST","GET"]) 
-#def search():
-#    cursor = conn.cursor()
-#    if request.method == 'POST':
-#        
-#        searchbox = request.form["searchbox"]
-#        #if searchbox == '':
-#        #    dropdown()
-        #else:
-
-    #     cursor.execute("SELECT Team FROM Auton WHERE Team LIKE '{}%' ORDER BY Match_Number".format(searchbox))
-    #     search_auton = cursor.fetchall()
-    #     cursor.execute("SELECT Team FROM Teleop WHERE Team LIKE '{}%' ORDER BY Match_Number".format(searchbox))
-    #     search_teleop = cursor.fetchall()
-    #     cursor.execute("SELECT Team FROM Endgame WHERE Team LIKE '{}%' ORDER BY Match_Number".format(searchbox))
-    #     search_endgame = cursor.fetchall()
-    #     cursor.execute("SELECT Team FROM Defense WHERE Team LIKE '{}%' ORDER BY Match_Number".format(searchbox))
-    #     search_defense = cursor.fetchall()
-    #     cursor.execute("SELECT Team FROM Defense WHERE Team LIKE '{}%' ORDER BY Match_Number".format(searchbox))
-    #     search_comments = cursor.fetchall()
-    # return jsonify({'htmlresponse': render_template('display.html', auton_1=search_auton, comments_1=search_comments, defense_1=search_defense, endgame_1=search_endgame, teleop_1=search_teleop)})
 
 if __name__ == '__main__':
    app.run()
